9e2640105df848e254aea3584ddf06ff1eaeca4b
unknown <dsed@pc-b042-04.die>
2021-11-12 14:08:31 +0100

    datapath

--- dsed_audio/dsed_audio.srcs/sources_1/new/FSMD_microphone.vhd
+++ dsed_audio/dsed_audio.srcs/sources_1/new/FSMD_microphone.vhd
@@ -44,6 +44,9 @@ Port ( clk_12megas : in STD_LOGIC;
 end FSMD_microphone;
 
 architecture Behavioral of FSMD_microphone is
+type state is (idle, data1, data2, ready_dato1, ready_dato2, reg_dato1, reg_dato2, not_ready, cuenta_sum, new_cycle);
+signal state_reg, state_next : state;
+signal sample_out_reg, sample_out_next : STD_LOGIC_VECTOR (sample_size-1 downto 0);
 signal dato1_reg, dato1_next, dato2_reg, dato2_next : unsigned(sample_size - 1 downto 0);
 signal cuenta_reg, cuenta_next : unsigned(8 downto 0);
 signal fst_cycle_reg, fst_cycle_next : std_logic;
@@ -56,54 +59,115 @@ begin
             dato1_reg <= (others=>'0');
             dato2_reg <= (others=>'0');
             fst_cycle_reg <= '0';
+            sample_out_reg <=  (others => '0');
         else
-            cuenta_reg <= cuenta_next;
+            state_reg <= state_next;
+            fst_cycle_reg <= fst_cycle_next;
             dato1_reg <= dato1_next;
             dato2_reg <= dato2_next;
-            fst_cycle_reg <= fst_cycle_next;    
+            cuenta_reg <= cuenta_next;
+            sample_out_reg <= sample_out_next;
         end if;    
     end if;
 end process;
 
 --next state logic and data path --
-process(micro_data, cuenta_reg, fst_cycle_reg)
-begin 
-if ((0 <= cuenta_reg and cuenta_reg <= 105) or (150 <= cuenta_reg and cuenta_reg <= 255)) then 
-    cuenta_next <= cuenta_reg + 1;
-    if (micro_data = '1') then
-        dato1_next <= dato1_reg + 1;
-        dato2_next <= dato2_reg + 1;
-    end if;
-elsif (106 <= cuenta_reg and cuenta_reg <= 149) then
-    cuenta_next <= cuenta_reg + 1;
-    if (micro_data = '1') then
-            dato1_next <= dato1_reg + 1;
-    end if;
-    if (fst_cycle_reg = '1' and cuenta_reg = 106) then
-        sample_out <= std_logic_vector(dato2_reg);
-        dato2_next <= (others => '0');
-        sample_out_ready <= enable_4_cycles;
-    else
-        sample_out_ready <= '0';
-    end if;
-else 
-    if (cuenta_reg = 299) then
-        cuenta_next <= (others =>'0');
-        fst_cycle_next <= '1';
-     else
-        cuenta_next <= cuenta_reg + 1;
-     end if;
-     if (micro_data = '1') then
-        dato2_next <= dato2_reg + 1;
-     end if;
-     
-     if (cuenta_reg = 256) then 
-        sample_out <= std_logic_vector(dato1_reg);     
-        dato1_next <= (others => '0');
-        sample_out_ready <= enable_4_cycles;    
-     else
-        sample_out_ready <= '0';
-     end if;
-end if;
+process(state_reg, cuenta_reg, reset, fst_cycle_reg )
+begin
+state_next <= state_reg;
+    case state_reg is
+        when idle =>
+            if(reset = '0') then
+                if ((0 <= cuenta_reg and cuenta_reg <= 105) or (150 <= cuenta_reg and cuenta_reg <= 255)) then
+                    state_next <= data1;
+                 elsif (106 <= cuenta_reg  and cuenta_reg <= 149) then
+                    state_next <= data2;
+                 elsif (cuenta_reg = 299) then
+                        state_next <= new_cycle;
+                 else 
+                        state_next <= cuenta_sum;
+                end if;
+            end if;
+        when data2 =>
+            state_next <= ready_dato2;
+        when ready_dato2 =>
+            if (fst_cycle_reg = '1' and cuenta_reg = 106) then
+                state_next <= reg_dato2;
+             else
+                state_next <= not_ready;
+             end if;
+        when new_cycle =>
+            state_next <= ready_dato1;
+        when  cuenta_sum =>
+            state_next <= ready_dato1;
+         when ready_dato1 => 
+            if (cuenta_reg = 256) then
+                state_next <= reg_dato1;
+            else
+                state_next <= not_ready;
+             end if;
+        when others =>
+            state_next <= idle;
+        end case;      
 end process;
+
+Mealyprocess : process(state_reg, cuenta_reg, micro_data, fst_cycle_reg, dato1_reg, dato2_reg)
+begin
+dato1_next <= dato1_reg;
+dato2_next <= dato2_reg;
+    case state_reg is
+        when idle =>
+        when data1 =>
+            if (micro_data = '1') then
+                dato1_next <= dato1_reg + 1;
+                dato2_next <= dato2_reg + 1;
+            end if;
+         when data2 =>
+            if (micro_data = '1') then
+                dato1_next <= dato1_reg + 1;
+            end if;
+        when ready_dato2 =>
+            if (cuenta_reg = 106) then
+                dato2_next <= (others => '0');
+             end if;
+        when new_cycle =>
+            if (micro_data = '1') then
+                dato2_next <= dato2_reg + 1;
+            end if;
+        when ready_dato1 =>
+            if(cuenta_reg = 256) then
+                dato1_next <= (others => '0');
+            end if;
+        when others =>
+        end case;
+end process;
+
+Moore_outpur : process(state_reg, cuenta_reg, dato1_reg, dato2_reg)
+begin
+cuenta_next <= cuenta_reg;
+sample_out_next <= sample_out_reg;
+sample_out_ready <= '0';
+fst_cycle_next <= fst_cycle_reg;
+    case state_reg is 
+        when data1 =>
+            cuenta_next <= cuenta_reg + 1;
+         when data2 =>
+            cuenta_next <= cuenta_reg + 1;
+          when reg_dato2 =>
+            sample_out_next <= std_logic_vector(dato2_reg);
+            sample_out_ready <= enable_4_cycles;
+          when reg_dato1=>
+              sample_out_next <= std_logic_vector(dato1_reg);
+              sample_out_ready <= enable_4_cycles;
+           when not_ready =>
+                sample_out_ready <= '0';
+            when new_cycle =>
+                cuenta_next <= (others => '0');
+                fst_cycle_next <= '1';
+            when cuenta_sum =>
+                cuenta_next <= cuenta_reg + 1;
+            when others =>
+        end case;                  
+end process;
+sample_out <= sample_out_reg;
 end Behavioral;
